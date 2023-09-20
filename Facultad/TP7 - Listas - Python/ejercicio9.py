dec5887bf2cda8fe75d4cf01c5d4aadc172a4a2a
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2023-09-20 10:10:44 -0300

    update

--- Facultad/TP7 - Listas - Python/ejercicio9.py
+++ Facultad/TP7 - Listas - Python/ejercicio9.py
@@ -14,15 +14,38 @@ def recorrer_lista(tamanio : int, vector : list):
     for indice in range(0, tamanio):
         print(f"{indice=} valor={vector[indice]}")
 
+def paridad(tamanio : str):
+    if (tamanio%2) == 0:
+        par = tamanio // 2
+        impar = par
+    else:
+        impar = tamanio // 2
+        par = impar + 1
+    return par, impar
+
+
 def media_par(tamanio : int, vector : list):
     acumulador = 0
+    
     for indice in range(0, tamanio, 2):
         acumulador += vector[indice]
-    return acumulador
+    a, b = paridad(tamanio)   
+    return acumulador / a
 
 
 if __name__=="__main__":
-    lista_numero = [4,3,1,4,5]
-    
-    print(len(lista_numero)//2)
+    lista_numero = [4,3,4,4,4,3]
+    # par: 0, 2 impar: 1, 3
+    # print((len(lista_numero)//2))
 
+    # if (len(lista_numero)%2) == 0:
+    #     par = len(lista_numero)//2
+    #     impar = par
+    #     print(f"{par=} {impar=}")
+    # else:
+    #     impar = len(lista_numero)//2
+    #     par = impar + 1
+        
+    #     print(f"{par=} {impar=}")
+
+    print(media_par(6,lista_numero))
