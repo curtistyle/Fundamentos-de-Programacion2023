a17bed860461c4e6c6569463deef64d2a56d11e0
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2023-09-23 21:15:39 -0300

    update initial repo

--- Facultad/TP9 - Metodos de ordenamiento/ejercicio1.py
+++ Facultad/TP9 - Metodos de ordenamiento/ejercicio1.py
@@ -1,15 +1,29 @@
-def crear_lista(tamanio : int):
-    vector = [None] * tamanio
+""" Hacer un algoritmo que:
+        - Lea una lista de números de teclado que culmina con uno negativo.
+        - Los ordene en forma creciente y Visualice la lista ordenada.
+        - Buscar si existe el Nº 27 en la lista. """
+
+def crear_lista(tamanio : int, valor=None):
+    vector = [valor] * tamanio
     return vector, tamanio
 
 def cargar_lista_dinamica(hasta):
     vector = []
     valor = int(input("Ingrese un valor: "))
     while valor != hasta:
+        print(vector)
         vector = vector + [valor]
         valor = int(input("Ingrese un valor: "))
-        print(vector)
     return vector, len(vector)
+
+def cargar_lista(vector, indice=0, hasta=0):
+    num = int(input(f"({indice})Ingrese un numero: "))
+
+    while ((num != hasta) and (indice < len(vector))):
+        vector[indice] = num
+        indice += 1
+        num = int(input(f"({indice})Ingrese un numero: "))
+    return indice
 
 def recorrer_lista(tamanio : int, vector : list):
     print("Elementos de la lista: ")
@@ -29,13 +43,13 @@ def busqueda_binaria(lista, tamanio, buscado, posicion=0):
     primero = 0
     ultimo  = tamanio - 1
     while (posicion == 0) and (primero <= ultimo):
-        medio = (primero + ultimo)//2
-        if lista[medio]==buscado:
+        medio = (primero + ultimo) // 2
+        if (lista[medio] == buscado):
             posicion = medio
-        elif lista[medio]>=buscado:
-            ultimo = medio - 1
+        elif (lista[medio] >= buscado):
+            ultimo   = medio - 1
         else:
-            primero = medio + 1   
+            primero  = medio + 1   
     return posicion
 
 
@@ -46,7 +60,7 @@ if __name__=="__main__":
 
     burbuja(lista_numeros)
 
-    tamanio = len(lista_numeros)
+    # tamanio = len(lista_numeros)
 
     recorrer_lista(tamanio, lista_numeros)
 
---
+++ Facultad/TP9 - Metodos de ordenamiento/ejercicio2.py
@@ -0,0 +1,6 @@
+""" Se lee un vector de Números enteros y se desea saber si entre dichos números se encuentra
+    un valor leído previamente. En caso afirmativo, visualizar su posición en la lista. Resolver el
+    problema por:
+        a- Búsqueda secuencial.
+        b- Búsqueda binaria. """
+
