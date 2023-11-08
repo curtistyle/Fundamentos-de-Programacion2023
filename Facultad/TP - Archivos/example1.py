0d463eb57adccf56d47f3472991356c9d31d5f13
seb_cep_12@hotmail.com <seb_cep_12@hotmail.com>
2023-11-08 18:21:44 -0300

    update

--- Facultad/TP - Archivos/example1.py
+++ Facultad/TP - Archivos/example1.py
@@ -1,25 +1,29 @@
-import json
+# import json
 
 
-with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as item:
-    for texto in item.readlines():
-        print(texto, end="")
+# with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as item:
+#     for texto in item.readlines():
+#         print(texto, end="")
     
-
-with open( "Facultad\TP - Archivos\main.txt", "a", encoding="utf-8" ) as item:
-    item.write("\n")
-    item.write("Nueva lieaa 1 \n")
-    item.write("Nueva lieaa 2 \n")
-    item.write("Nueva lieaa 3")
+# archivo = None
+# with open( "Facultad\TP - Archivos\main.txt", "a", encoding="utf-8" ) as item:
+#     item.writelines()
+#     archivo = item
     
-print(end="\n\n")
+# # print(end="\n\n")
 
-f = open( "Facultad\TP - Archivos\dyct.json", "r", encoding="utf-8" )
-print( type(f) )
+# f = open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" )
+# # print( type(f) )
 
-var = json.load(f)
-print(var)
 
-import io
+with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as fichero:
+    print(fichero)
+    print( f"{'#':<5} {'Nombre':>10} {'Edad':>10}" )
+    for linea in fichero:
+        numero, nombre, edad = linea.split()
+        print( f"{numero:<5} {nombre:>10} {edad:>10}" )
+    fichero.close()
 
-io.TextIOWrapper
+with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as fichero:
+    fichero.seek(0,0)
+    print(fichero.read())
