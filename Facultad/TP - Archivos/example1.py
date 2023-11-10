c00ba4d9eb290dff3a82034ce530f0243ab13058
Sebastian Maldonado <58012659+curtistyle@users.noreply.github.com>
2023-11-10 20:46:32 -0300

    update

--- Facultad/TP - Archivos/example1.py
+++ Facultad/TP - Archivos/example1.py
@@ -16,14 +16,20 @@
 # # print( type(f) )
 
 
-with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as fichero:
-    print(fichero)
-    print( f"{'#':<5} {'Nombre':>10} {'Edad':>10}" )
-    for linea in fichero:
-        numero, nombre, edad = linea.split()
-        print( f"{numero:<5} {nombre:>10} {edad:>10}" )
-    fichero.close()
+# with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as fichero:
+#     print(fichero)
+#     print( f"{'#':<5} {'Nombre':>10} {'Edad':>10}" )
+#     for linea in fichero:
+#         numero, nombre, edad = linea.split()
+#         print( f"{numero:<5} {nombre:>10} {edad:>10}" )
+#     fichero.close()
 
-with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as fichero:
-    fichero.seek(0,0)
-    print(fichero.read())
+# with open( "Facultad\TP - Archivos\main.txt", "r", encoding="utf-8" ) as fichero:
+#     fichero.seek(0,0)
+#     print(fichero.read())
+
+
+with open( "Facultad\TP - Archivos\cuentas.txt", "r", encoding="utf-8" ) as fichero:
+
+    
+    
